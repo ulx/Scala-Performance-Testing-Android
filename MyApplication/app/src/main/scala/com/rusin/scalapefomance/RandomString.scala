5574aef15cff16edd6c856fc4db3958d7da441f4
arusin <alexandr.rusin@gmail.com>
2015-03-24 09:46:48 +0300

    add project

---
+++ MyApplication/app/src/main/scala/com/rusin/scalapefomance/RandomString.scala
@@ -0,0 +1,11 @@
+package com.rusin.scalapefomance
+
+import java.util.Random
+import java.lang.Long
+
+class RandomString(i: Int, j: Int){
+    val r = new Random
+    val l = Math abs r.nextLong
+    val s = java.lang.Long.toString(l,36)
+    var num = i + j
+}
