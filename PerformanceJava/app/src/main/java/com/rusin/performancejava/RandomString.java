5db558b48d129d7b208803bf972213e09006f1c8
arusin <alexandr.rusin@gmail.com>
2015-04-01 19:49:21 +0300

    add java test

---
+++ PerformanceJava/app/src/main/java/com/rusin/performancejava/RandomString.java
@@ -0,0 +1,24 @@
+package com.rusin.performancejava;
+
+import java.util.Random;
+
+public class RandomString {
+	
+	private String s;
+	private int num;
+
+	public RandomString(int i, int j){
+    Random r = new Random();
+    long l = Math.abs(r.nextLong());
+    this.s = Long.toString(l,36);
+    this.num = i + j;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+
+	public String getS() {
+		return s;
+	}
+}
